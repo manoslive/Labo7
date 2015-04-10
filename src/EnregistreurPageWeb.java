18eeaaff0a26b6061b2313c3ecd5ffe3afd6c2ab
manoslive <ebeloin@hotmail.com>
2015-04-09 22:40:27 -0400

    Initial commit

---
+++ src/EnregistreurPageWeb.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Emmanuel Beloin & Shaun Cooper on 2015-04-09.
+ */
+public class EnregistreurPageWeb {
+    public void Enregistrer()
+    {
+
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
